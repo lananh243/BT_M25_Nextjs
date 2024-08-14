5772abb1a6d07e17abfaee76f451b8194b98a108
lan anh234lan anh 234 <nguyenthilananh21092005@gmail.com>
2024-08-14 11:16:29 +0700

    Bai tap

--- session02_03/my-app/next.config.mjs
+++ session02_03/my-app/next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "static.vecteezy.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.websiteessentials.com.au",
+      },
+      {
+        protocol: "https",
+        hostname: "img.ws.mms.shopee.vn",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
